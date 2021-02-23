54cf07ebde3f461f5a397cc2edaddb1e4d27b939
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-02-23 12:25:26 +0530

    basic blockchain module

---
+++ blockchain.py
@@ -0,0 +1,28 @@
+from block import Block
+
+
+class Blockchain:
+    """
+    Blockchain: a public ledger of transactions.
+    Implemented as a list of blocks - data sets of transaction
+    """
+
+    def __init__(self):
+        self.chain = []
+
+    def add_block(self, data):
+        self.chain.append(Block(data))
+
+    def __repr__(self):
+        return f"Blockchain:{self.chain}"
+
+
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('1')
+    blockchain.add_block('2')
+    print(blockchain)
+
+
+if __name__ == '__main__':
+    main()
