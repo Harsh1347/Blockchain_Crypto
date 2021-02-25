e45edb8a2bf71e0d7992edc434bda2cbfeafba18
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-02-25 14:31:26 +0530

    blockchain for collab completed

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -16,6 +16,24 @@ class Blockchain:
     def __repr__(self):
         return f"Blockchain:{self.chain}"
 
+    def replace_chain(self, chain):
+        """"
+        Replace the local chain with incoming one if the following applies:
+        - The incoming chain must be longer than the local.
+        - The incoming chain is formatted properly.
+        """
+        if len(chain) <= len(self.chain):
+            raise Exception(
+                'Cannot replace. The incoming chain must be longer')
+
+        try:
+            Blockchain.is_valid_chain(chain)
+        except Exception as e:
+            raise Exception(
+                f'Cannot replace. The incoming chain is invalid: {e}')
+
+        self.chain = chain
+
     @staticmethod
     def is_valid_chain(chain):
         """
