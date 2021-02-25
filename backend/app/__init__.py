5f46dbfcd77438d5efd47b25994cdddf5a51bfdb
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-02-25 15:12:46 +0530

    blockchain/mine endpoint added

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -19,4 +19,11 @@ def route_blockchain():
     return jsonify(blockchain.to_json())
 
 
+@app.route('/blockchain/mine')
+def route_blockchain_mine():
+    transaction_data = 'stubbed_transaction_data'
+    blockchain.add_block(transaction_data)
+    return jsonify(blockchain.chain[-1].to_json())
+
+
 app.run()
