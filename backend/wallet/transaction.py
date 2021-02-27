4b973e8d3871aba1db7f9fea4ddce42a26517ff7
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-02-28 02:19:12 +0530

    transaction update test validate

--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -45,6 +45,40 @@ class Transaction:
             'signature': sender_wallet.sign(output)
         }
 
+    def update(self, sender_wallet, recipient, amount):
+        """
+        Update the transaction with a existing or new recipient.
+        """
+        if amount > self.output[sender_wallet.address]:
+            raise Exception('Amount exceeds balance')
+
+        if recipient in self.output:
+            self.output[recipient] = self.output[recipient] + amount
+        else:
+            self.output[recipient] = amount
+
+        self.output[sender_wallet.address] = self.output[sender_wallet.address] - amount
+
+        self.input = self.create_input(sender_wallet, self.output)
+
+    @staticmethod
+    def is_valid_transaction(transaction):
+        """
+        Validate a transaction.
+        Raise an exception for invalid transactions.
+        """
+        output_total = sum(transaction.output.values())
+
+        if transaction.input['amount'] != output_total:
+            raise Exception('Invalid transaction output values')
+
+        if not Wallet.verify(
+            transaction.input['public_key'],
+            transaction.output,
+            transaction.input['signature']
+        ):
+            raise Exception('Invalid signature')
+
 
 def main():
     transaction = Transaction(Wallet(), 'recipient', 15)
