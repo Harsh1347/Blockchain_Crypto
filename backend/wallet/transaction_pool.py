429188e742bf7460a7e0d3b35cfc938254edc5c3
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-03-01 21:35:33 +0530

    transaction pool implemented

---
+++ backend/wallet/transaction_pool.py
@@ -0,0 +1,9 @@
+class TransactionPool:
+    def __init__(self):
+        self.transaction_map = {}
+
+    def set_transaction(self, transaction):
+        """
+        Set a transaction in transaction pool.
+        """
+        self.transaction_map[transaction.id] = transaction
