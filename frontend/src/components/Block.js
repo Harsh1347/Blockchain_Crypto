3bd735577cea2e78f0a166d4b2c91fc0f1682811
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-03-04 00:17:34 +0530

    Transaction component added

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MILLISECONDS_PY } from '../config'
+import Transaction from './Transaction'
 
 function Block({ block }) {
 
@@ -11,7 +12,14 @@ function Block({ block }) {
         <div className="Block">
             <div>Hash: {hashDisplay}</div>
             <div>Timestamp: {timestampDisplay}</div>
-            <div>Data: {JSON.stringify(data)}</div>
+            <div>
+                {data.map(transaction => (
+                    <div key={transaction.id}>
+                        <hr />
+                        <Transaction transaction={transaction} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
