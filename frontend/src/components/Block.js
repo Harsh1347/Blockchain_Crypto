86bc9690180d2e0616fbcbecb8286e5ce973aca5
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-03-04 13:11:34 +0530

    Toggle feature to block component added

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import { MILLISECONDS_PY } from '../config'
-import Transaction from './Transaction'
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+
+import { MILLISECONDS_PY } from '../config';
+import Transaction from './Transaction';
+
+function ToggleTransactionDisplay({ block }) {
+    const [DisplayTransaction, setDisplayTransaction] = useState(false)
+    const { data } = block;
+
+    const toggleDisplayTransaction = () => {
+        setDisplayTransaction(!DisplayTransaction)
+    }
+
+    if (DisplayTransaction) {
+        return (
+            <div>
+                {
+                    data.map(transaction => (
+                        <div key={transaction.id}>
+                            <hr />
+                            <Transaction transaction={transaction} />
+                        </div>
+                    ))
+                }
+                <br />
+                <Button variant='Danger' size='sm' onClick={toggleDisplayTransaction}>
+                    Show Less
+                </Button>
+            </div>
+        )
+
+    }
+    return (
+        <div>
+            <br />
+            <Button variant="danger" size='sm' onClick={toggleDisplayTransaction}>
+                Show More
+            </Button>
+        </div>
+    )
+}
 
 function Block({ block }) {
 
-    const { timestamp, hash, data } = block;
+    const { timestamp, hash } = block;
     const hashDisplay = `${hash.substring(0, 15)}...`
     const timestampDisplay = new Date(timestamp / MILLISECONDS_PY).toLocaleString();
 
@@ -12,14 +52,7 @@ function Block({ block }) {
         <div className="Block">
             <div>Hash: {hashDisplay}</div>
             <div>Timestamp: {timestampDisplay}</div>
-            <div>
-                {data.map(transaction => (
-                    <div key={transaction.id}>
-                        <hr />
-                        <Transaction transaction={transaction} />
-                    </div>
-                ))}
-            </div>
+            <ToggleTransactionDisplay block={block} />
         </div>
     )
 }
